b9b7c95adb47be0ca77834a8e11c354afb7c5ed9
MFAudiovisual <marcosp.musical@gmail.com>
2023-03-03 19:16:12 -0300

    SiteSys SPA Template

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+import { useStorage } from '@vueuse/core'
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+const state = useStorage('vueuse-color-scheme')
+
+onMounted(() => {
+  if (!state.value === 'light') {
+    useDark()
+  }
+})
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/thanks-contact',
+      name: 'thanks-contact',
+      component: () => import('@/views/ThanksContactView.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('@/views/NotFound.vue'),
+    },
+  ],
+})
+
+export default router
